28fb70bdb67dde84c07429d37d94c75134820697
VeelisVA <veelisva@gmail.com>
2025-08-13 01:45:19 +0300

    Added Tests

---
+++ Tests/SilosTests.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using SilosApi.Controllers;
+using SilosApi.Dto;
+using SilosApi.Services;
+
+namespace Tests;
+
+public class SilosTests
+{
+    [Fact]
+    public async Task GetUserName_ReturnsNameFromService()
+    {
+        // Arrange
+        var mockUserService = new Mock<ISilosService>();
+        mockUserService
+            .Setup(x => x.GetAllSilosAsync(null))
+            .ReturnsAsync(new List<SilosDto>());
+    
+        var controller = new SilosController(mockUserService.Object);
+    
+        // Act
+        var result = await controller.GetAllSilos(null);
+    
+        // Assert
+        //result.Should().Be(new List<SilosDto>());
+        Assert.IsType<OkObjectResult>(result.Result);
+        
+        mockUserService.Verify(x => x.GetAllSilosAsync(null), Times.Once);  // Проверка, что метод вызван 1 раз
+    }
+}
